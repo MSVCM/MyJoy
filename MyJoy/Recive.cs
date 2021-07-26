3d07ee1011f5c33e1c794647c1aa9ad4046b8879
MSVCM <MSVCM@DESKTOP-6BQVQTT>
2021-07-26 19:54:07 +0800

    添加项目文件。

---
+++ MyJoy/Recive.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+
+namespace MyJoy
+{
+    public class Receive
+    {
+        public Socket client;
+        public string result;
+        public Thread receiveThread;
+
+        public float time { get; set; }
+
+        public int num = 0;
+
+        public Receive(Socket c,int i,Thread t)
+        {
+            client = c;
+            num = i;
+            receiveThread = t;
+            time = 5;
+        }
+
+    }
+}
